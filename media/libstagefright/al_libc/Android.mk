a197ab15b2ef559717499b35b6b417c029ce4959
iBullRay <bullray097@yandex.ru>
2015-11-16 16:08:06 +0200

    media: Fixed Makefile for al_libc

--- media/libstagefright/al_libc/Android.mk
+++ media/libstagefright/al_libc/Android.mk
@@ -1,8 +1,8 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
+LOCAL_SRC_FILES := \
     al_libc.c \
     al_uconv.c \
     al_detect.cpp \
@@ -15,7 +15,20 @@ LOCAL_C_INCLUDES := \
 LOCAL_SHARED_LIBRARIES := libutils libcutils libicuuc libion
 
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE:= libalc
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_MODULE := libalc
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    al_libc.c
+
+LOCAL_C_INCLUDES := \
+    $(call include-path-for, alsp) \
+    $(TOP)/frameworks/av/include/alsp/inc
+
+LOCAL_MODULE := libalc
 include $(BUILD_STATIC_LIBRARY)
